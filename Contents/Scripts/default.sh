dba88aae7eb999c9206cae1c10ddc106de6ad0bd
Quang-Linh LE <ql.le@hotmail.com>
2017-10-20 15:26:34 +0200

    Binary via composer

--- Contents/Scripts/default.sh
+++ Contents/Scripts/default.sh
@@ -4,5 +4,5 @@
 #
 
 export PATH="/usr/local/bin:$PATH"
-fake=$(php /Users/link/opt/faker-cli/bin/faker.php -c1 $*)
+fake=$(php $HOME/.composer/vendor/bin/faker.php -c1 $*)
 osascript -e "tell application \"LaunchBar\" to paste in frontmost application \"$fake\"" &
